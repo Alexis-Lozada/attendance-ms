321d1436b9e727233c5c2bcff6962530293b453c
Alexis Lozada <alexislozadasalinas11@gmail.com>
2025-10-07 14:01:10 -0600

    Implement microservice for uploading images/files

---
+++ storage-ms/src/main/java/mx/edu/uteq/idgs12/storage_ms/controller/FileStorageController.java
@@ -0,0 +1,40 @@
+package mx.edu.uteq.idgs12.storage_ms.controller;
+
+import mx.edu.uteq.idgs12.storage_ms.dto.FileDTO;
+import mx.edu.uteq.idgs12.storage_ms.entity.FileStorage;
+import mx.edu.uteq.idgs12.storage_ms.service.FileStorageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileStorageController {
+
+    private final FileStorageService fileService;
+
+    public FileStorageController(FileStorageService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+public ResponseEntity<FileDTO> uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
+    System.out.println("📦 Archivo recibido: " + file.getOriginalFilename());
+    return ResponseEntity.ok(fileService.uploadFile(file));
+}
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<FileStorage> getFile(@PathVariable String uuid) {
+        return fileService.getFileByUuid(uuid)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<Void> deleteFile(@PathVariable String uuid) {
+        fileService.deleteFile(uuid);
+        return ResponseEntity.noContent().build();
+    }
+}
